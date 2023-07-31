493a50d5ce4bb227e91010c683028e36a4f2a9cb
julioarturo <32986837+julioarturo@users.noreply.github.com>
2023-07-31 09:06:41 -0600

    Revert "test port"
    This reverts commit c9c5bfd2d3c516b8b8462a85998ea08ae37373a1.

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const express = require("express");
 const path = require('path');
 
 const app = express();
-const port = process.env.PORT || 801;
+const port = process.env.PORT || 80;
 const index_direction = process.env.INDEX_DIRECTION;
 
 app.get("/", function (req, res) {
