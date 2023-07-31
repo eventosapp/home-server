49576bf38977b34411b8feefe4cf5705f1e8ff18
julioarturo <32986837+julioarturo@users.noreply.github.com>
2023-07-31 09:04:50 -0600

    Add path

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 // Server with express
 const express = require("express");
+const path = require('path');
+
 const app = express();
 const port = process.env.PORT || 80;
 const index_direction = process.env.INDEX_DIRECTION;
